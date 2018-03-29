acb4f80ffdbceaef7b76a38d08bee005631c5e16
prometheus61 <btceeds@gmail.com>
2018-03-29 20:18:08 +0100

    first 3 exercises

---
+++ ex1.py
@@ -0,0 +1,6 @@
+print "Hello World!"
+#print "Hello Again!"
+print "I like typing this"
+#print "this is fun"
+print "Ok enough now I get the idea"
+# print 'I "said" do not do that'
---
+++ ex2.py
@@ -0,0 +1,5 @@
+# this is a comment
+print "this should be printed" # another comment which will not be visible
+print "and we are back to printing text again"
+# print "this line is commented out"
+print "this line runs because there is no # before it"
